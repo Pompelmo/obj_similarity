e5ed61c4927e75cbdbfa61d779ec0e86420a7737
Pompelmo <gallo.kate@gmail.com>
2015-09-15 10:12:57 +0200

    add try/except

--- tf_idf/top_similar.py
+++ tf_idf/top_similar.py
@@ -2,7 +2,7 @@ import pickle
 import gensim
 import url_id_to_text as uitt
 
-tfidf = gensim.models.TfidfModel.load('source/web_text_tifidf_2.tfidf_model')
+tfidf = gensim.models.TfidfModel.load('source/web_text_tifidf.tfidf_model')
 index = gensim.similarities.Similarity.load('source/tfidfSim.index')
 dictionary = gensim.corpora.Dictionary.load('source/web_text_dict.dict')
 
@@ -12,11 +12,16 @@ output.close()
 
 
 def most_similar(url_id):
-    text = uitt.transform(url_id)                   # given in input a website, return token/stem test
+    try:
+        text = uitt.transform(url_id)                   # given in input a website, return token/stem test
+    except IndexError:
+        print "this website is not in the index, try again..."
+        return None
+
     txtbow = dictionary.doc2bow(text)
     vector = tfidf[txtbow]                            # return tfidf vector of the text
     sims = index[vector]
-    sims = sorted(enumerate(sims), key=lambda item: -item[1])  # compute similarity,
+    sims = sorted(enumerate(sims), key=lambda item: -item[1])[:10]  # compute similarity,
     for ite in sims:
         print ite, mean_dict[ite[0]]
 
@@ -28,6 +33,7 @@ def main():
     while word != "stop":
 
         most_similar(word)
+
         print "input a website to compute 10 most similar websites according to tf_idf, or 'stop' to exit"
         word = raw_input("--> ")
 
