e64552220b5ad4931c96682b402ac69f6c5f6cd3
Pompelmo <gallo.kate@gmail.com>
2015-09-18 16:12:24 +0200

    part of the previous text_corpus

--- tf_idf/text_tfidf.py
+++ tf_idf/text_tfidf.py
@@ -3,9 +3,9 @@ import logging
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)  # print info
 
-dictionary = gensim.corpora.Dictionary.load('source/web_text_dict.dict')
+dictionary = gensim.corpora.Dictionary.load('source/web_text_dict_last.dict')
 
-corpus = gensim.corpora.MmCorpus('source/web_text_mm_2.mm')  # load the bow corpus
+corpus = gensim.corpora.MmCorpus('source/web_text_mm_last.mm')  # load the bow corpus
 
 print corpus  # it prints number of documents, of features (unique tokens) and non-zero entities
 
@@ -13,8 +13,8 @@ tfidf = gensim.models.TfidfModel(corpus, normalize=True)    # initialize a model
 
 corpus_tfidf = tfidf[corpus]  # create a wrapper around the old corpus document stream
 
-tfidf.save('source/web_text_tifidf.tfidf_model')  # save tfidf model
+tfidf.save('source/web_text_tifidf_last.tfidf_model')  # save tfidf model
 
-index = gensim.similarities.Similarity('source/similarity_index', corpus_tfidf, num_features=len(dictionary))
+index = gensim.similarities.Similarity('source/similarity_index_last', corpus_tfidf, num_features=len(dictionary))
 
-index.save('source/tfidfSim.index')  # save the similarity matrix
+index.save('source/tfidfSim_last.index')  # save the similarity matrix
