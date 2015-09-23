3f42e0051cd7254f3c1f52db4fffef23270c91fa
Pompelmo <gallo.kate@gmail.com>
2015-09-23 12:16:36 +0200

    add datetime to have an idea of time taken

--- integration/loading.py
+++ integration/loading.py
@@ -5,34 +5,37 @@
 
 import pickle
 import gensim
-import numpy as np
-from sklearn.neighbors import NearestNeighbors
+from datetime import datetime
 
 
 def loading():
 
-    print "loading model for tf-idf"
+    print datetime.now(), "loading model for tf-idf"
     tfidf = gensim.models.TfidfModel.load('source/web_text_tifidf_last.tfidf_model')
     index = gensim.similarities.Similarity.load('source/tfidfSim_last.index')
     tfidf_dict = gensim.corpora.Dictionary.load('source/web_text_dict_last.dict')
 
-    print "loading dictionary for tf-idf"
+    print datetime.now(), "loading dictionary for tf-idf"
     tfidf_dict_file = open('source/web_text_doc_last.pkl', 'r')
     tfidf_web = pickle.load(tfidf_dict_file)
     tfidf_dict_file.close()
 
-    print "loading dictionary for w2v keywords n_similarity"
+    print datetime.now(), "loading dictionary for w2v keywords n_similarity"
     mean_dict_file = open('source/mean_dict_key_scan.pkl', 'r')
     mean_dict = pickle.load(mean_dict_file)
     mean_dict_file.close()
 
-    print "computing nearest neighbor ball tree"
-    values = mean_dict.values()
-    arrays = np.array(values)
-    nbrs = NearestNeighbors(n_neighbors=11, radius=1.0, algorithm='ball_tree', p=2, metric='euclidean')
-    nbrs.fit(arrays)
+    print datetime.now(), "loading ball tree for nearest neighbor"
+    input_file = open("source/ball_tree", "r")
+    ball_tree = pickle.load(input_file)
+    input_file.close()
 
-    print "loading d2v description model"
+    print datetime.now(), "loading d2v description model"
     d2v_model = gensim.models.Doc2Vec.load('source/d2vdescription')
 
-    return tfidf, index, tfidf_dict, tfidf_web, mean_dict, nbrs, d2v_model
+    print datetime.now(), "loading word2vector model"
+    w2v_model = gensim.models.Word2Vec.load('source/w2vmodel_keywords_scan')
+
+    print datetime.now(), "load finish"
+
+    return tfidf, index, tfidf_dict, tfidf_web, mean_dict, ball_tree, w2v_model, d2v_model
