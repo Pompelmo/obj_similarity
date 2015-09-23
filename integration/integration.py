ddd8a8e7fdf221ca0a94a9a932a0a659f1077561
Pompelmo <gallo.kate@gmail.com>
2015-09-23 12:16:56 +0200

    added parameter for simplification

--- integration/integration.py
+++ integration/integration.py
@@ -6,19 +6,19 @@
 
 import url_id_to_text as uitt
 from how_many import Counter
-from gensim.models import Word2Vec
+from math import sqrt
 
 
 class Integration(object):
-    def __init__(self, tfidf, index, tfidf_dict, tfidf_web, mean_dict, nrbs, d2v_model):
+    def __init__(self, tfidf, index, tfidf_dict, tfidf_web, mean_dict, ball_tree, w2v_model, d2v_model):
         self.tfidf = tfidf                      # tfidf model
         self.index = index                      # tfidf similarity matrix
         self.tfidf_dict = tfidf_dict            # dictionary for word <-> id
         self.tfidf_web = tfidf_web              # dictionary for doc n <-> website
-        self.nrbs = nrbs                        # nearest neighbors ball tree structure
+        self.ball_tree = ball_tree              # nearest neighbors ball tree structure
         self.mean_dict = mean_dict              # mean vector <-> website
         self.d2v_model = d2v_model              # description doc2vec model
-        self.w2v_model = Word2Vec.load('source/w2vmodel_keywords_scan')                 # keywords word2vec model
+        self.w2v_model = w2v_model              # keywords word2vec model
         self.cc = Counter(self.w2v_model, self.d2v_model, self.tfidf_dict, self.tfidf)  # counter for keywords/token
 
     def ms_tfidf(self, url_id, n):
@@ -31,7 +31,7 @@ class Integration(object):
         txtbow = self.tfidf_dict.doc2bow(text)              # transform in numeric vector
         vector = self.tfidf[txtbow]                         # return tfidf vector of the text
         sims = self.index[vector]                           # return similarity matrix index
-        sims = sorted(enumerate(sims), key=lambda item: -item[1])[:n]  # compute similarity
+        sims = sorted(enumerate(sims), key=lambda item: -item[1])[:n+1]  # compute similarity
 
         rank = []
         scores = []
@@ -39,9 +39,12 @@ class Integration(object):
 
         for ite in sims:
             url = self.tfidf_web[ite[0]]                    # find document website name
-            rank.append(url)                                # append website name
-            scores.append(ite[1])                           # append score (the higher the better)
-            length_text.append(self.cc.count_text(url))     # count non zero tfidf tokens
+            if url != url_id:
+                rank.append(url)                                # append website name
+                cosine_sim = float(ite[1])                      # cosine similarity
+                dist = sqrt(2.0 * (1.0 - cosine_sim)) / 2.0     # transform cosine similarity in euclidean distance
+                scores.append(dist)                             # append score (the smaller the better)
+                length_text.append(self.cc.count_text(url))     # count non zero tfidf tokens
 
         return scores, rank, length_text
 
@@ -53,7 +56,7 @@ class Integration(object):
             return [""]*n, [""]*n, [0]*n                # otherwise return empty rank and empty score
 
         # compute the nearest neighbors with the constructed ball_tree
-        distance, index = self.nrbs.kneighbors([value], n_neighbors=n+1, return_distance=True)
+        distance, index = self.ball_tree.query([value], k=n+1, return_distance=True, sort_results=True)
 
         # transform the result in lists
         dist = distance.tolist()[0]
@@ -71,8 +74,8 @@ class Integration(object):
             if keys[ind[i]] != url_id:              # do not return the same website
                 url = keys[ind[i]]
                 rank.append(url)                    # append website name
-                scores.append(dist[i])              # append distance
-                length_token.append(self.cc.count_keywords(url))
+                scores.append(dist[i] / 2.0)          # append normalized distance
+                length_token.append(len(self.cc.count_keywords(url)))
 
         return scores, rank, length_token
 
@@ -81,15 +84,17 @@ class Integration(object):
         try:
             ms = self.d2v_model.docvecs.most_similar(url_id, topn=n)        # compute most similar with d2v
         except KeyError:
-            return [""]*n, [""]*n          # if the website is not present in the model, return empty rank and scores
+            return [""]*n, [""]*n, [0]*n     # if the website is not present in the model, return empty rank and scores
 
         rank = []
         scores = []
         length_token = []
 
         for item in ms:
-            rank.append(item[0])                    # compute rank and scores list
-            scores.append(item[1])
-            length_token.append(self.cc.count_description(item[0]))
+            rank.append(item[0])                              # compute rank and scores list
+            cosine_sim = item[1]
+            dist = sqrt(2.0 * (1.0 - cosine_sim)) / 2.0       # transform cosine similarity in euclidean distance
+            scores.append(dist)
+            length_token.append(len(self.cc.count_description(item[0])))
 
         return scores, rank, length_token
