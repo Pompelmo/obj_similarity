68fc833a77afed24887ab8d8d06643e22e769937
Pompelmo <gallo.kate@gmail.com>
2015-11-19 09:17:15 +0100

    added db connections

--- integration/integration.py
+++ integration/integration.py
@@ -8,18 +8,23 @@ from vectors_mean import *
 
 
 class Integration(object):
-    def __init__(self, corpus, tfidf, index, tfidf_web, mean_dict, ball_tree, d2v_model):
+    def __init__(self, corpus, tfidf, index, tfidf_web, db_mean_value, ball_tree, id_to_web, d2v_model):
         self.corpus = corpus                    # bow corpus
         self.tfidf = tfidf                      # tfidf model
         self.index = index                      # tfidf similarity matrix
         self.tfidf_web = tfidf_web              # dictionary for doc n <-> website
-        self.mean_dict = mean_dict              # mean vector <-> website
+        self.db_mean_value = db_mean_value      # mean vector <-> website database
         self.ball_tree = ball_tree              # nearest neighbors ball tree structure
+        self.id_to_web = id_to_web
         self.d2v_model = d2v_model              # description doc2vec model
 
     def ms_tfidf(self, weblist, n):
         """compute most similar websites using tfidf text model"""
         mean_vector, number = mean_tfidf(self.tfidf_web, self.corpus, self.tfidf, weblist)
+
+        if number == 0:
+            return [], []
+
         number += n
         sims = self.index[mean_vector][:number]                      # query for similarity
 
@@ -39,7 +44,10 @@ class Integration(object):
     def ms_w2v_key(self, weblist, n):
         """compute most similar websites using w2v keywords model"""
 
-        mean_vec_w2v, number = mean_w2v(self.mean_dict, weblist)
+        mean_vec_w2v, number = mean_w2v(self.db_mean_value, weblist)
+
+        if number == 0:
+            return [], []
         # compute the nearest neighbors with the constructed ball_tree
         number += n
         distance, index = self.ball_tree.query([mean_vec_w2v], k=number, return_distance=True, sort_results=True)
@@ -48,7 +56,6 @@ class Integration(object):
         dist = distance.tolist()[0]
         ind = index.tolist()[0]
 
-        keys = self.mean_dict.keys()
         # have the list of websites names:
         # recall that dict.keys()[i] = key and dict.values()[i] = value are such that dict[key] = value
 
@@ -56,8 +63,8 @@ class Integration(object):
         scores = []
 
         for i in range(0, len(dist)):
-            if keys[ind[i]] not in weblist:              # do not return the same website
-                url = keys[ind[i]]
+            if self.id_to_web[ind[i]] not in weblist:              # do not return the same website
+                url = self.id_to_web[ind[i]]
                 rank.append(url)                    # append website name
                 scores.append(dist[i] / 2.0)          # append normalized distance
 
@@ -67,6 +74,10 @@ class Integration(object):
         """compute the most similar websites using d2v descriptions model"""
 
         mean_vec, number = mean_d2v(self.d2v_model, weblist)
+
+        if number == 0:
+            return [], []
+
         rank = []
         scores = []
 
